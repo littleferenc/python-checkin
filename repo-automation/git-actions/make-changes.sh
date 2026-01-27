0f23c6fab4c4d5241224d092a0d731e2049cfbf0
littleferenc <frantisekturcik@gmail.com>
2026-01-27 11:32:47 +0300

    Automaticka zmena 2026-01-27 11:32:47

--- repo-automation/git-actions/make-changes.sh
+++ repo-automation/git-actions/make-changes.sh
@@ -5,7 +5,7 @@ FILE="../zmeny.txt"
 cas_na_masine=$(date | awk '{print $4}' | awk -F ":" '{print $1}')
 sk_cas=$(($cas_na_masine - 2))
 
-echo "Zmena vytvorena: $(date | awk '{print $1}') o case $sk_cas" > $FILE
+echo "Zmena vytvorena: $(date | awk -F " " '{print $1}') o case $sk_cas" > $FILE
 echo "Random hash: $(openssl rand -hex 8)" >> $FILE
 
 echo "Subor zmeny.txt bol vytvoreny/aktualizovany"
